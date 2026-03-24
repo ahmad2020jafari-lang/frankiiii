5b3235732e2c9a4f95c9965bf4c4ad3e0fe7b653
Ahmad Jafari <ahmadjafari@Ahmads-MacBook-Pro.local>
2026-03-24 14:31:52 +0100

    Setup Express server with basic routing

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// That will be my backend File
+// server.js - Basic Express setup
+const express = require("express");
+const http = require("http");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("TicTacToe Server Running");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Server running on port " + PORT));
